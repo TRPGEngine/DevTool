295aac55996d59fa6fcac7c12cfe581214f1b349
moonrailgun <moonrailgun@gmail.com>
2019-02-05 18:16:21 +0800

    :star: trpg basic config

---
+++ script/config/trpg.js
@@ -0,0 +1,20 @@
+module.exports = {
+  db: {
+    database: 'trpg',
+    username: 'root',
+    password: '',
+    options: {
+      localhost: 'localhost',
+      dialect: 'mysql',
+      operatorsAliases: false,
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+      },
+    }
+  },
+  webserviceHomepage: '/admin/home',
+  apihost: 'http://127.0.0.1:23256',
+}
