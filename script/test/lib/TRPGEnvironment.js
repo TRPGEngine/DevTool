b2ade36f36cf6514acf34fdab3c3e7cb6baeef02
moonrailgun <moonrailgun@gmail.com>
2019-02-07 17:08:06 +0800

    :fire: testEvent

--- script/test/lib/TRPGEnvironment.js
+++ script/test/lib/TRPGEnvironment.js
@@ -1,6 +1,7 @@
 const NodeEnvironment = require('jest-environment-node');
 const config = require('../../config/trpg');
 const trpgapp = require('../../server');
+const db = trpgapp.storage.db;
 
 class TRPGEnvironment extends NodeEnvironment {
   constructor(config) {
@@ -12,7 +13,29 @@ class TRPGEnvironment extends NodeEnvironment {
 
     // 声明沙盒内可用的全局变量
     this.global.trpgapp = trpgapp;
-    this.global.db = trpgapp.storage.db;
+    this.global.db = db;
+    this.global.testEvent = (eventFn, data) => {
+      // 测试发送socket时事件数据方法
+      return new Promise(resolve, reject) {
+        try {
+          let ret = await eventFn(data, (_res) => {
+            resolve(_res)
+          }, db);
+
+          if(ret !== undefined) {
+            if(typeof ret === 'boolean') {
+              resolve({result: ret});
+            }else if(typeof ret === 'object' && typeof ret.result !== 'boolean') {
+              resolve(Object.assign({result: true}, ret))
+            }else {
+              resolve(ret);
+            }
+          }
+        }catch(e) {
+          resolve({result: false, msg: e.toString()})
+        }
+      }
+    }
 
     await super.setup();
   }
