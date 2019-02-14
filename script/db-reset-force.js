209dcbca8dd86f52916e4fc207d6920f48cd28d3
moonrailgun <moonrailgun@gmail.com>
2019-02-14 15:21:28 +0800

    修改重置数据库的调用脚本

--- script/db-reset-force.js
+++ script/db-reset-force.js
@@ -2,7 +2,7 @@ const server = require('./server');
 
 (async function() {
   console.log('重置应用服务...');
-  await server.reset();
+  await server.reset({force: true});
   await server.close();
   console.log('重置应用服务完毕,等待应用自动退出或手动退出');
 })()
