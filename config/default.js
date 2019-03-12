70bc1b2065cbe6e306bb9a0c83c24ccd5cc420df
moonrailgun <moonrailgun@gmail.com>
2019-03-12 10:07:14 +0800

    :hammer: 增加默认配置

--- config/default.js
+++ config/default.js
@@ -25,5 +25,26 @@ module.exports = {
     }]
   },
   webserviceHomepage: '/admin/home',
-  apihost: 'http://127.0.0.1:23256'
+  apihost: 'http://127.0.0.1:23256',
+  oauth: {
+    // qq互联相关信息 required!
+    qqconnect: {
+      "appid": "",
+      "appkey": "",
+      "callback": "/oauth/qq/callback",
+      "scope": ["get_user_info"]
+    }
+  },
+  mail: {
+    aeskey: "", // 32位秘钥
+    smtp: {
+      host: "",
+      port: 465,
+      secure: true,
+      auth: {
+        user: "",
+        pass: ""
+      }
+    }
+  }
 }
